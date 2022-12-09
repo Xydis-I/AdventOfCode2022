86b907f6081c1ec72fb4f264381b2468fc2fe4f5
Xydis-I <ccb9952@hotmail.com>
2022-12-09 02:10:48 -0330

    Day 6

---
+++ AdventOfCode_Day06/Part1.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode_Day06
+{
+    internal static class Part1
+    {
+        public static void Solution()
+        {
+            const string fileLocation = @"C:\Users\ccb99\source\repos\AdventOfCode2022\Day06_Input.txt";
+
+            try
+            {
+                using var input = new StreamReader(fileLocation);
+                var line = input.ReadToEnd();
+
+                for (var i = 3; i < line.Length; i++)
+                {
+                    var hash = new HashSet<char>();
+                    for (int j = 0; j < 4; j++)
+                        hash.Add(line[i - j]);
+
+                    if (hash.Count != 4) continue;
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ AdventOfCode_Day06/Part2.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode_Day06
+{
+    internal static class Part2
+    {
+        public static void Solution()
+        {
+            const string fileLocation = @"C:\Users\ccb99\source\repos\AdventOfCode2022\Day06_Input.txt";
+
+            try
+            {
+                using var input = new StreamReader(fileLocation);
+                var line = input.ReadToEnd();
+
+                for (var i = 13; i < line.Length; i++)
+                {
+                    var hash = new HashSet<char>();
+                    for (int j = 0; j < 14; j++)
+                        hash.Add(line[i - j]);
+
+                    if (hash.Count != 14) continue;
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
